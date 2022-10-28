4488b8be83417abc94d7fb07727170c478c932b4
Fernando José Lavarreda Urizar <fernandolavarredau@gmail.com>
2022-10-28 09:57:25 -0600

    Writing to CSVs

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"os"
 	"encoding/csv"
-	"fmt"
 	"strconv"
+	"fmt"
 	"io"
-	"log"
+	"bufio"
+	"strings"
 )
 
 
@@ -20,8 +21,10 @@ type Record struct{
 	Format []string
 }
 
+
 func count(arr []string, value string) int{
 	mapd := make(map[string]int)
+	mapd[value] = 0;
 	for _, v := range arr{
 		if _, in := mapd[v]; in{
 			mapd[v] += 1
@@ -31,6 +34,7 @@ func count(arr []string, value string) int{
 	}
 	return mapd[value]
 }
+
 
 func (record *Record) readArr(str []string) Record{
 	INITCAP_INT := count(record.Format, "INT")
@@ -66,10 +70,41 @@ func (record *Record) readArr(str []string) Record{
 }
 
 
+func (record *Record) writeArr() []string{
+	size := len(record.Ints) + len(record.Floats) + len(record.Strings);
+	str_arr := make([]string, size)
+	
+	curints := 0
+	curfloats := 0
+	curstrings := 0
+	
+	
+	var reading string;
+	for i, v := range record.Format{
+		
+		switch ;v{
+		case PARSING[0]:
+			reading = strconv.Itoa(record.Ints[curints])
+			curints+=1
+		case PARSING[1]:
+			reading = fmt.Sprintf("%v", record.Floats[curfloats])
+			curfloats+=1
+		case PARSING[2]:
+			reading = record.Strings[curstrings]
+			curstrings+=1
+			fmt.Println("LETSSSS GOOO")
+		}
+		str_arr[i] = reading
+	}
+	
+	return str_arr;
+}
+
+
 func CheckColumns(fileloc string, delimiter rune) (int, []string, []string) {
 	file, err := os.Open(fileloc)
 	if err != nil{
-		log.Fatal(err);
+		return -1, []string{""}, []string{""}
 	}
 	reader := csv.NewReader(file)
 	reader.Comma = delimiter
@@ -79,15 +114,16 @@ func CheckColumns(fileloc string, delimiter rune) (int, []string, []string) {
 }
 
 
-func ReadCSV(fileloc string, delimiter rune, format []string, header bool) ([]Record, []int){
-	template := Record{Format: format}
+func (template *Record) ReadCSV(fileloc string, delimiter rune, header bool) ([]Record, []int){
 	records := make([]Record, INIT_RECORDS)
 	cursor := 0;
 	errors := make([]int, 10)
 	file, err := os.Open(fileloc)
 	if err != nil{
-		log.Fatal(err);
+		errors[0] = -1
+		return []Record{}, errors 
 	}
+	defer file.Close()
 	reader := csv.NewReader(file)
 	reader.Comma = delimiter
 	if header{
@@ -113,11 +149,35 @@ func ReadCSV(fileloc string, delimiter rune, format []string, header bool) ([]Re
 }
 
 
+func WriteCSV(fileloc string, delimiter string, header []string, records []Record) (int){
+	file, err := os.Create(fileloc)
+	if err != nil{
+		return -1
+	}
+	
+	defer file.Close()
+	dw := bufio.NewWriter(file)
+	
+	if len(header) != 0{
+		dw.WriteString(strings.Join(header, delimiter)+"\n");
+	}
+
+	for _, v := range records{
+		dw.WriteString(strings.Join(v.writeArr(), delimiter)+"\n");
+	}
+
+	dw.Flush()
+	return 0
+}
+
+
+
 func main(){
 	rec := Record{Format: []string{"INT", "FLOAT", "FLOAT", "FLOAT", "FLOAT"}};
 	fmt.Println(CheckColumns("cv.txt", '\t'))
-	records, fails := ReadCSV("cv.txt", '\t', rec.Format, true)
+	records, _ := rec.ReadCSV("cv.txt", '\t', true)
 	fmt.Println(records)
-	fmt.Println(fails)
-
+	fmt.Println(records[0].writeArr())
+	value := WriteCSV("cv1.txt", ",", []string{}, records)
+	fmt.Println(value)
 }
